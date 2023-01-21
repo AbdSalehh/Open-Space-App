05f4d5bc8202ce5fff73d7754e3868f8f403c312
Abd. Saleh <abdshaleh124@webmail.umm.ac.id>
2023-01-21 22:18:18 +0700

    add ci action

--- src/components/RegisterInput.js
+++ src/components/RegisterInput.js
@@ -3,22 +3,22 @@ import PropTypes from 'prop-types';
 import useInput from '../hooks/useInput';
 
 function RegisterInput({ register }) {
-  const [name, onNameChange] = useInput('');
-  const [id, onIdChange] = useInput('');
-  const [password, onPasswordChange] = useInput('');
+    const [name, onNameChange] = useInput('');
+    const [id, onIdChange] = useInput('');
+    const [password, onPasswordChange] = useInput('');
 
-  return (
-    <form className="register-input">
-      <input type="text" value={name} onChange={onNameChange} placeholder="Name" />
-      <input type="text" value={id} onChange={onIdChange} placeholder="Username" />
-      <input type="password" value={password} onChange={onPasswordChange} placeholder="Password" />
-      <button type="button" onClick={() => register({ name, id, password })}>Register</button>
-    </form>
-  );
+    return (
+        <form className="register-input">
+            <input type="text" value={name} onChange={onNameChange} placeholder="Name" />
+            <input type="text" value={id} onChange={onIdChange} placeholder="Username" />
+            <input type="password" value={password} onChange={onPasswordChange} placeholder="Password" />
+            <button type="button" onClick={() => register({ name, id, password })}>Register</button>
+        </form>
+    );
 }
 
 RegisterInput.propTypes = {
-  register: PropTypes.func.isRequired,
+    register: PropTypes.func.isRequired,
 };
 
 export default RegisterInput;
